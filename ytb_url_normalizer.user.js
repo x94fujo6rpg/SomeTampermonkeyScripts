c46c6284034ebadb35ed4c05e08b52ea8344e1da
x94fujo6rpg <x94fujo6rpg@gmail.com>
2020-12-04 00:20:41 +0800

    fixed

--- ytb_url_normalizer.user.js
+++ ytb_url_normalizer.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/x94fujo6rpg/SomeTampermonkeyScripts
 // @updateURL    https://github.com/x94fujo6rpg/SomeTampermonkeyScripts/raw/master/ytb_url_normalizer.user.js
 // @downloadURL  https://github.com/x94fujo6rpg/SomeTampermonkeyScripts/raw/master/ytb_url_normalizer.user.js
-// @version      0.01
+// @version      0.02
 // @description  normalize url in video description / remove miniplayer (still activated when you watch in playlist)
 // @author       x94fujo6
 // @match        https://www.youtube.com/*
@@ -14,19 +14,19 @@
     'use strict';
     let location;
     let cid;
-    window.onload = function () {
+    window.onload = setTimeout(function () {
         console.log("script start");
         location = window.location.href;
         main();
         removeMiniplayer();
         setInterval(watcher, 1000);
-    };
+    }, 3000);
 
     function removeMiniplayer() {
         let b = document.querySelectorAll(".ytp-miniplayer-button");
-        if (b) b.forEach(e => { e.remove(); console.log(e); });
-        b = document.querySelectorAll("script[src*='miniplayer.js']");
-        if (b) b.forEach(e => { e.remove(); console.log(e); });
+        if (b) b.forEach(e => { e.remove(); });
+        b = document.querySelectorAll("[src*='miniplayer.js']");
+        if (b) b.forEach(e => { e.remove(); });
     }
 
     function watcher() {
@@ -62,65 +62,38 @@
 
         let links = document.querySelectorAll("a.yt-formatted-string");
         let count = 0;
-        links.forEach(e => {
-            let mass = decodeURIComponent(e.href).replace("https://www.youtube.com/redirect?", "");
-            if (mass.includes("redir_token")) {
-                let token = getToken(mass);
-                if (!token) return console.log("token not found. script stop.");
-                let url = mass
-                    .replace(`&v=${id}`, "")
-                    .replace(`v=${id}`, "")
-                    .replace(`&redir_token=${token}`, "")
-                    .replace(`redir_token=${token}`, "")
-                    .replace(`&event=video_description`, "")
-                    .replace(`event=video_description`, "")
-                    .replace("&q=http", "http")
-                    .replace("q=http", "http")
-                    ;
-                e.href = url;
-                console.log(`set urlReplacer on url: ${url}`);
-                let interID = setInterval(() => urlReplacer(e, url, 1, interID, c_location), 500);
-                count++;
-            }
+        links.forEach(a => {
+            if (!a.href.includes("redirect?")) return;
+            let url = getLink(a.href);
+            if (!url) return;
+            url = decodeURIComponent(url);
+            console.log(`set urlReplacer on url: ${url}`);
+            let interID = setInterval(() => urlReplacer(a, url, interID, c_location), 500);
+            a.addEventListener("mouseover", () => { urlReplacer(a, url, interID, c_location); });
+            count++;
         });
         console.log(`script end. found ${count} url`);
     }
 
-    function urlReplacer(ele, url, code, interID, c_location) {
+    function getLink(href) {
+        let reg = /&q=(.[^\&]*)/;
+        let extract = reg.exec(href);
+        return extract ? extract[1] : false;
+    }
+
+    function urlReplacer(ele, url, interID, c_location) {
         if (c_location != window.location.href) {
             console.log(`page change. urlReplacer:${interID} will stop`);
             return clearInterval(interID);
         }
         if (ele.href != url) {
-            let type = {
-                1: "change",
-                2: "mouseover",
-                3: "select",
-            };
-            console.log(`link change detected. type:${type[code]} re-replace:${url}`);
+            //console.log(`link change detected. re-replace:${url}`);
             ele.href = url;
         }
     }
 
     function getVideoID() {
         let pos = document.querySelector("ytd-watch-flexy");
-        if (pos) {
-            return pos.getAttribute("video-id");
-        } else {
-            return false;
-        }
-    }
-
-    function getToken(url) {
-        let index = url.indexOf("&redir_token=");
-        if (index === -1) index = url.indexOf("redir_token=");
-        if (index === -1) return false;
-        let token = url.slice(index);
-        token = token
-            .replace("&redir_token=", "")
-            .replace("redir_token=", "")
-            ;
-        token = token.split("&")[0];
-        return token;
+        return pos ? pos.getAttribute("video-id") : false;
     }
 })();
