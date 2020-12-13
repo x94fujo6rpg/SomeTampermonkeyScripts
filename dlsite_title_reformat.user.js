7ee036044fb8f0f0c678cf16e812ba40417157c5
x94fujo6rpg <x94fujo6rpg@gmail.com>
2020-12-14 06:37:40 +0800

    offset

--- dlsite_title_reformat.user.js
+++ dlsite_title_reformat.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/x94fujo6rpg/SomeTampermonkeyScripts
 // @updateURL    https://github.com/x94fujo6rpg/SomeTampermonkeyScripts/raw/master/dlsite_title_reformat.user.js
 // @downloadURL  https://github.com/x94fujo6rpg/SomeTampermonkeyScripts/raw/master/dlsite_title_reformat.user.js
-// @version      0.62
+// @version      0.63
 // @description  remove title link / remove excess text / custom title format / click button to copy
 // @author       x94fujo6
 // @match        https://www.dlsite.com/maniax/work/=/product_id/*
