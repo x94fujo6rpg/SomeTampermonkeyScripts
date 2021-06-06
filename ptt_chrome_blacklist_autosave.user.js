e493cadb3964efecc2c07966630bc469836e15f8
x94fujo6rpg <x94fujo6rpg@gmail.com>
2021-06-06 17:44:54 +0800

    log

--- ptt_chrome_blacklist_autosave.user.js
+++ ptt_chrome_blacklist_autosave.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/x94fujo6rpg/SomeTampermonkeyScripts
 // @updateURL    https://github.com/x94fujo6rpg/SomeTampermonkeyScripts/raw/master/ptt_chrome_blacklist_autosave.user.js
 // @downloadURL  https://github.com/x94fujo6rpg/SomeTampermonkeyScripts/raw/master/ptt_chrome_blacklist_autosave.user.js
-// @version      0.01
+// @version      0.02
 // @description  autosave blacklist (use Tampermonkey API GM_setValue & GM_getValue)
 // @author       x94fujo6
 // @match        https://iamchucky.github.io/PttChrome/*
@@ -14,12 +14,13 @@
 /* jshint esversion: 9 */
 
 (async function () {
-	let retry = 0,
+	let
 		blacklist = false,
 		addUser = false,
 		removeUser = false;
 	const
 		listKey = "blackList",
+		log = (...any) => console.log(`%c[autosave blacklist]%c`, "color:OrangeRed;", "", ...any),
 		setGMValue = (key = listKey, value = "") => GM_setValue(key, value),
 		getGMValue = (key = listKey, default_value = false) => GM_getValue(key, default_value),
 		getListInApp = () => {
@@ -32,7 +33,7 @@
 			let data = getListInApp();
 			if (!data.length) return;
 			setGMValue(listKey, data);
-			console.log(`autosave blacklist [${data.split("\n").length}]`);
+			log(`autosave blacklist [${data.split("\n").length}]`);
 		},
 		loadList = () => {
 			let data = getGMValue(),
@@ -41,31 +42,30 @@
 			if (!data.length) return;
 			blacklist.value = data;
 			data = data.split("\n");
-			console.log(`load blacklist [${data.length}]`);
+			log(`load blacklist [${data.length}]`);
 			data.forEach(id => {
 				if (list[id]) return;
 				list[id] = true;
 				count++;
 			});
-			console.log(`add missing id [${count}]`);
+			log(`added missing id [${count}]`);
 		},
-		start = () => {
-			if (!blacklist) return console.log("blacklist not found");
+		start = () => {			
+			log(`script start`);
 			blacklist.addEventListener("input", autoSave);
 			addUser.addEventListener("click", autoSave);
 			removeUser.addEventListener("click", autoSave);
 			loadList();
 		},
-		wait = () => {
+		wait = (retry = 30) => {
 			blacklist = document.querySelector("#opt_blacklistedUsers");
 			addUser = document.querySelector("#cmenu_addBlacklistUserId");
 			removeUser = document.querySelector("#cmenu_removeBlacklistUserId");
-			if ((!blacklist || !addUser || !removeUser) && retry <= 30) {
-				retry++;
-				console.log(`target not found, retry [${retry}]`);
-				setTimeout(wait, 1000);
+			if ((!blacklist || !addUser || !removeUser) && retry > 0) {
+				retry--;
+				log(`target not found, remaining retries [${retry}]`);
+				setTimeout(() => wait(retry), 1000);
 			} else {
-				console.log(`script start`);
 				setTimeout(start, 1000);
 			}
 		};
