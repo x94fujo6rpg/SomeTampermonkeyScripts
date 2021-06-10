6ecfc591cc247d5b3b7a441900fbb180752209b7
x94fujo6rpg <x94fujo6rpg@gmail.com>
2021-06-11 03:11:34 +0800

    add new tab options

--- all_link_to_new_tab.user.js
+++ all_link_to_new_tab.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         open all link in new tab
+// @name         open all links in new tab
 // @namespace    https://github.com/x94fujo6rpg/SomeTampermonkeyScripts
 // @updateURL    https://github.com/x94fujo6rpg/SomeTampermonkeyScripts/raw/master/all_link_to_new_tab.user.js
 // @downloadURL  https://github.com/x94fujo6rpg/SomeTampermonkeyScripts/raw/master/all_link_to_new_tab.user.js
 // @version      0.03
-// @description  make all link open in new tab
+// @description  open all links in new tab
 // @author       x94fujo6
 // @match        *://*/*
 // @grant        GM_openInTab
@@ -18,7 +18,7 @@
 		/**
 		 * active: decides whether the new tab should be focused, (是否在新分頁打開後跳到該分頁)
 		 * insert: that inserts the new tab after the current one, (是否將新分頁排在目前分頁後面)
-		 * setParent: makes the browser re-focus the current tab on close and (是否在該分頁關閉時回到目前分頁)
+		 * setParent: makes the browser re-focus the current tab on close (是否在該分頁關閉時回到目前分頁)
 		 * incognito: makes the tab being opened inside a incognito mode/private mode window. (是否在隱密模式下打開分頁)
 		 */
 		newTabConfig = {
