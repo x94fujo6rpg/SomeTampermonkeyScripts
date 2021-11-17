708741b77e39d99a2223765a59e5c0cdfef1ea3c
x94fujo6rpg <x94fujo6rpg@gmail.com>
2021-11-17 11:18:08 +0800

    fix the misalignment of headr when the page is loaded in the background (wiki's own issue)

--- az_cn_wiki_fleet_tech_tool.user.js
+++ az_cn_wiki_fleet_tech_tool.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/x94fujo6rpg/SomeTampermonkeyScripts
 // @updateURL    https://github.com/x94fujo6rpg/SomeTampermonkeyScripts/raw/master/az_cn_wiki_fleet_tech_tool.user.js
 // @downloadURL  https://github.com/x94fujo6rpg/SomeTampermonkeyScripts/raw/master/az_cn_wiki_fleet_tech_tool.user.js
-// @version      0.01
+// @version      0.02
 // @description  海事局的艦隊科技頁面 可以點擊編號來標記已120的船
 // @author       x94fujo6
 // @match        https://wiki.biligame.com/blhx/%E8%88%B0%E9%98%9F%E7%A7%91%E6%8A%80
@@ -11,23 +11,58 @@
 // @grant        GM_setValue
 // ==/UserScript==
 
+/**
+ * [changelog]
+ * 
+ * 0.02
+ * 自動修復當頁面在背景中載入後標題列的錯位問題 (WIKI本身問題 關掉腳本一樣會)
+ * https://i.imgur.com/kQWlEU1.jpg
+ * 
+ */
+
 (function () {
 	'use strict';
 	const
 		key = { ship_id: "ship_id", },
 		bg_color = "gray",
 		getValue = () => GM_getValue(key.ship_id, []),
-		setValue = (list) => GM_setValue(key.ship_id, (list instanceof Array) ? list : []);
-	wait();
+		setValue = (list) => GM_setValue(key.ship_id, (list instanceof Array) ? list : []),
+		log = (...any) => console.log(`%c[碧航艦隊科技工具]%c`, "color:OrangeRed;", "", ...any),
+		sleep = (ms = 0) => new Promise(resolve => setTimeout(resolve, ms));
 
-	function wait(retry = 30) {
-		let target = document.querySelector("#CardSelectTr");
-		if (!target && retry > 0) {
-			retry--;
-			log(`target not found, remaining retries [${retry}]`);
-			setTimeout(() => wait(retry), 500);
-		} else {
-			setTimeout(main, 500);
+	waitPageLoad(30, "#CardSelectTr", main);
+
+	async function waitPageLoad(retry = 30, selector = "", run = () => { }, delay = 1000) {
+		await waitTab();
+		waitEle();
+
+		function waitTab() {
+			return new Promise(resolve => {
+				if (document.visibilityState === "visible") return resolve();
+				log("tab in background, script paused");
+				document.addEventListener("visibilitychange", () => {
+					if (document.visibilityState === "visible") {
+						log("script unpaused");
+						return resolve();
+					}
+				});
+			});
+		}
+
+		async function waitEle() {
+			if (selector.length) {
+				let target = document.querySelector(selector);
+				if (!target && retry > 0) {
+					retry--;
+					log(`target not found, remaining retries [${retry}]`);
+					setTimeout(() => waitEle(retry), 500);
+				} else {
+					await sleep(1000);
+					run();
+				}
+			} else {
+				throw Error("selector is empty");
+			}
 		}
 	}
 
@@ -35,7 +70,17 @@
 		let
 			pos = document.querySelector("#CardSelectTr"),
 			table = pos.querySelector("tbody"),
-			msg = document.createElement("div");
+			msg = document.createElement("div"),
+			changeColor = (eles, color = "") => {
+				eles.forEach(ele => ele.style.backgroundColor = color);
+			},
+			updateList = (list) => {
+				document.querySelector("#wiki_tool_marked_count").textContent = list.size;
+				document.querySelector("#wiki_tool_marked_list").value = [...list];
+			};
+
+		fixDataHeader();
+
 		table.children.forEach(tr => addSwitch([...tr.children], tr.children[0]));
 		msg.innerHTML = `
 			<div style="display: inline-flex;">
@@ -60,13 +105,7 @@
 		document.querySelector("#wiki_tool_save").onclick = () => {
 			let
 				_list = new Set(document.querySelector("#wiki_tool_marked_list").value.split(",")),
-				table = document.querySelector("#CardSelectTr>tbody"),
-				changeColor = (eles, color = "") => {
-					eles.forEach(ele => ele.style.backgroundColor = color);
-				},
-				updateList = (list) => {
-					document.querySelector("#wiki_tool_marked_count").textContent = list.size;
-				};
+				table = document.querySelector("#CardSelectTr>tbody");
 			_list.delete('');
 			log("載入清單", [..._list]);
 			setValue([..._list]);
@@ -78,7 +117,23 @@
 			});
 			updateList(_list);
 		};
-		log(getValue());
+		log("載入清單", getValue());
+
+		function fixDataHeader() {
+			let head = document.querySelector("#CardSelectTr>thead");
+			if (head.children.length == 1) {
+				let bottom = document.querySelector("#CardSelectTr>tbody>tr");
+				log("missing head, trying to fix it");
+				if (!(bottom.innerHTML.match(/dataHeader headerSort/gm))) {
+					throw Error(`element not found, abort\n${bottom.innerHTML}`);
+				} else {
+					head.appendChild(bottom);
+					log("head fixed");
+				}
+			} else {
+				log("normal datahead");
+			}
+		}
 
 		function addSwitch(elelist, id) {
 			id.onclick = () => checkList();
@@ -94,13 +149,6 @@
 						_list.delete(`${_id}`);
 						setValue([..._list]);
 					},
-					changeColor = (color = "") => {
-						elelist.forEach(ele => ele.style.backgroundColor = color);
-					},
-					updateList = (list) => {
-						document.querySelector("#wiki_tool_marked_count").textContent = list.size;
-						document.querySelector("#wiki_tool_marked_list").value = [...list];
-					},
 					_list = new Set(getValue()),
 					_id = id.innerText.trim(),
 					_name = elelist[1].innerText.trim(),
@@ -110,27 +158,23 @@
 					if (!ini) {
 						await removeFromList(_id);
 						log(`remove ${_id} ${_name}, list size:${_list.size}`);
-						changeColor();
+						changeColor(elelist);
 						updateList(_list);
 					} else {
-						changeColor(bg_color);
+						changeColor(elelist, bg_color);
 					}
 				} else {
 					if (!ini) {
 						await addToList(_id);
 						log(`add ${_id} ${_name}, list size:${_list.size}`);
-						changeColor(bg_color);
+						changeColor(elelist, bg_color);
 						updateList(_list);
 					} else {
-						changeColor();
+						changeColor(elelist);
 					}
 				}
 			}
 		}
-	}
-
-	function log(...any) {
-		console.log(`%c[碧航艦隊科技工具]%c`, "color:OrangeRed;", "", ...any);
 	}
 })();
 
