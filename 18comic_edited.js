17a98f5509fe101526e8611e5171dcd5fa691535
x94fujo6rpg <x94fujo6rpg@gmail.com>
2022-08-20 20:01:36 +0800

    fix 18comic script

--- 18comic_edited.js
+++ 18comic_edited.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         18comic漫画下载edited
 // @namespace    http://github.com/eternalphane/Userscripts/
-// @version      1.0.5.1
+// @version      1.0.5.2
 // @description  从18comic上下载cbz格式（整话阅读）或webp格式（分页阅读）的漫画
 // @author       eternalphane (edit by x94fujo6)
 // @license      MIT
@@ -25,9 +25,9 @@
 // ==/UserScript==
 
 (async () => {
-	const btn = document.querySelector('.menu-bolock i.fa-download').parentElement;
+	let btn = document.querySelector('.menu-bolock i.fa-download').parentElement;
 	if (location.search) {
-		return btn.addEventListener('click', async e => {
+		btn.addEventListener('click', async e => {
 			e.preventDefault();
 			const page = document.querySelector('div[id*=".jpg"],div[id*=".webp"]');
 			save(
@@ -35,22 +35,24 @@
 				`${page.id.slice(0, -4)}.webp`
 			);
 		});
+		return;
 	}
 	GM.addStyle(await GM.getResourceText('css'));
-	const overlay = document.createElement('div');
+
+	let overlay = document.createElement('div');
 	overlay.id = 'dl-overlay';
 	overlay.innerHTML = await GM.getResourceText('html');
 	overlay.hidden = true;
 	document.body.appendChild(overlay);
 
-	const circle = overlay.querySelectorAll('circle')[1];
-	const number = overlay.querySelector('span');
-	const msg = overlay.querySelector('h2');
-	const updateProgress = (percent, text) => {
-		circle.style.strokeDasharray = `${percent} 100`;
-		number.innerText = Math.round(percent);
-		text != undefined && (msg.innerText = text);
-	};
+	let circle = overlay.querySelectorAll('circle')[1],
+		number = overlay.querySelector('span'),
+		msg = overlay.querySelector('h2'),
+		updateProgress = (percent, text) => {
+			circle.style.strokeDasharray = `${percent} 100`;
+			number.innerText = Math.round(percent);
+			if (text != undefined) msg.innerText = text;
+		};
 
 	btn.addEventListener('click', async e => {
 		e.preventDefault();
@@ -59,72 +61,126 @@
 		}
 		overlay.hidden = false;
 		document.body.classList.add('noscroll');
+
 		const pages = [...document.querySelectorAll('div[id*=".jpg"],div[id*=".webp"]')];
 		const total = pages.length;
+
 		let progress = 0;
 		updateProgress(0, 'Downloading...');
-		const zip = new JSZip;
+
+		const zip = new JSZip();
 		await Promise.all(pages.map(async page => {
-			zip.file(`${page.id.slice(0, -4)}.webp`, await download(
-				page.querySelector('img').getAttribute('data-original')
-			));
+			zip.file(
+				`${page.id.slice(0, -4)}.webp`,
+				await download(page.querySelector('img').getAttribute('data-original'))
+			);
 			updateProgress(++progress * 100 / total);
 		}));
 		updateProgress(0, 'Compressing...');
+
 		// TODO: Select output format? (cbz, cbt, pdf)
 		save(
-			await zip.generateAsync({
-				type: 'blob',
-				compression: 'DEFLATE',
-				compressionOptions: { level: 9 },
-				mimeType: 'application/vnd.comicbook+zip'
-			}, (meta) => updateProgress(meta.percent)),
+			await zip.generateAsync(
+				{
+					type: 'blob',
+					compression: 'DEFLATE',
+					compressionOptions: { level: 9 },
+					mimeType: 'application/vnd.comicbook+zip'
+				}, (meta) => updateProgress(meta.percent)
+			),
 			`${document.querySelector('.panel-heading .pull-left').textContent.trim()}.cbz`
 		);
 		document.body.classList.remove('noscroll');
 		overlay.hidden = true;
 	});
-})();
 
-/**
- * @param {string} url
- */
-const download = async url => {
-	const img = new Image;
-	img.src = URL.createObjectURL((await new Promise((resolve, reject) => GM.xmlHttpRequest({
-		url,
-		method: 'GET',
-		responseType: 'blob',
-		onload: resolve,
-		onerror: reject
-	}))).response);
-	await new Promise((resolve, reject) => (img.onload = resolve, img.onerror = reject));
-	const canvas = document.createElement('canvas');
-	const ctx = canvas.getContext('2d');
-	const w = canvas.width = img.naturalWidth;
-	const h = canvas.height = img.naturalHeight;
-	// `aid`, `scramble_id` and `md5` are both global variables
-	if (!url.includes('.jpg') || aid < scramble_id) {
-		ctx.drawImage(img, 0, 0);
-	} else {
-		const num = (md5(`${aid}${url.slice(url.lastIndexOf('/') + 1).split('.')[0]}`).slice(-1).charCodeAt(0) % 10 + 1) * 2;
-		const rem = h % num;
-		const sh = Math.floor(h / num);
-		let sy = h - rem - sh, dy = rem;
-		ctx.drawImage(img, 0, sy, w, rem + sh, 0, 0, w, rem + sh);
-		for (let i = 1; i < num; ++i) {
-			ctx.drawImage(img, 0, sy -= sh, w, sh, 0, dy += sh, w, sh);
+	function get_num(aid, img_index) {
+		if (aid >= 268850) {
+			return ((md5(`${aid}${img_index}`).slice(-1).charCodeAt(0) % 10) + 1) * 2;
+		} else {
+			return 10;
 		}
 	}
-	URL.revokeObjectURL(img.src);
-	// TODO: Select image type? Change quality?
-	return new Promise(resolve => canvas.toBlob(resolve, 'image/webp', 0.9));
-};
 
-const save = (blob, filename) => {
-	const a = document.createElement('a');
-	a.href = URL.createObjectURL(blob);
-	a.download = filename;
-	a.click();
-	URL.revokeObjectURL(a.href);
-};
+	function get_img(url) {
+		return new Promise((resolve, reject) => {
+			GM.xmlHttpRequest(
+				{
+					url,
+					method: 'GET',
+					responseType: 'blob',
+					onload: resolve,
+					onerror: reject
+				}
+			);
+		});
+	}
+
+	const download = async url => {
+		let img = new Image(),
+			img_id = url.match(/\d+\..+$/)[0],
+			img_index = img_id.split(".")[0],
+			img_data,
+			num = get_num(aid, img_index),
+			canvas = document.createElement('canvas'),
+			canvas_2d = canvas.getContext('2d'),
+			img_nwidth = 0,
+			img_nheight = 0,
+			sWidth = 0;
+
+		img_data = await get_img(url);
+		img_data = img_data.response;
+		img.src = URL.createObjectURL(img_data);
+
+		await new Promise((resolve, reject) => (img.onload = resolve, img.onerror = reject));
+
+		img_nwidth = img.naturalWidth;
+		img_nheight = img.naturalHeight;
+		sWidth = img_nwidth;
+		canvas.width = img_nwidth;
+		canvas.height = img_nheight;
+
+		// `aid`, `scramble_id` and `md5` are both global variables
+		if (url.includes('.gif') || aid < scramble_id) {
+			canvas_2d.drawImage(img, 0, 0);
+		}
+
+		if (url.match(/\.jpg$/)) {
+			let _num = get_num(aid, img_index),
+				rem = img_nheight % _num,
+				sh = Math.floor(img_nheight / _num),
+				sy = img_nheight - rem - sh, dy = rem;
+			canvas_2d.drawImage(img, 0, sy, img_nwidth, rem + sh, 0, 0, img_nwidth, rem + sh);
+			for (let i = 1; i < _num; ++i) {
+				canvas_2d.drawImage(img, 0, sy -= sh, img_nwidth, sh, 0, dy += sh, img_nwidth, sh);
+			}
+		}
+
+		if (url.match(/\.webp$/)) {
+			let naturalHeight = parseInt(img_nheight % num);
+			for (i = 0; i < num; i++) {
+				let sHeight = Math.floor(img_nheight / num),
+					dy = sHeight * i,
+					sy = img_nheight - sHeight * (i + 1) - naturalHeight;
+				if (i == 0) {
+					sHeight += naturalHeight;
+				} else {
+					dy += naturalHeight;
+				}
+				canvas_2d.drawImage(img, 0, sy, sWidth, sHeight, 0, dy, sWidth, sHeight);
+			}
+		}
+
+		URL.revokeObjectURL(img.src);
+		// TODO: Select image type? Change quality?
+		return new Promise(resolve => canvas.toBlob(resolve, 'image/webp', 0.9));
+	};
+
+	const save = (blob, filename) => {
+		const a = document.createElement('a');
+		a.href = URL.createObjectURL(blob);
+		a.download = filename;
+		a.click();
+		URL.revokeObjectURL(a.href);
+	};
+})();
